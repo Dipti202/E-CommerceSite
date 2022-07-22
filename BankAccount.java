75329bced1e8467a5e30158d52fc5f6eacdfbdac
Dipti202 <109797337+Dipti202@users.noreply.github.com>
2022-07-22 15:51:14 +0530

    bank assignment
    bank assignment

---
+++ BankAccount.java
@@ -0,0 +1,34 @@
+package com.app.banking;
+import static utils
+public class BankAccount {
+	
+	private int acctNo;
+	
+	private String cName;
+	private AcType type
+private LocalDate createDate;
+private double balance;
+
+public BankAccount(int acctNo, String cName,Actype type LocalDate createDate, double balance) {
+	super();
+	this.acctNo = acctNo;
+	this.cName = cName;
+	this.type=type;
+	this.createDate = createDate;
+	this.balance = balance;
+}
+@Override
+public String toString() {
+	return "BankAccount [acctNo=" + acctNo + ", cName=" + cName + ", balance=" + balance + ", getClass()="
+			+ getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+}
+public void deposit(double amount)
+{
+	this.balance+=amount;
+}
+public void withdraw(double amount)
+{
+	this.balance-=amount;
+}
+
+}
